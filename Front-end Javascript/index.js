01e77e6ff2631f21d8abb3411ab1e4adcc233235
VictorKAS <victorkaynan21@gmail.com>
2025-07-30 00:07:04 -0300

    att

--- Front-end Javascript/index.js
+++ Front-end Javascript/index.js
@@ -21,10 +21,45 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const nomeItem = document.createElement("p");
     nomeItem.innerText = inputItem.value;
 
+    inputCheckbox.addEventListener('click', function() {
+        if (inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through";
+        }
+        else { 
+            nomeItem.style.textDecoration = "none";
+        }}
+    );        
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", 
+        { weekday: 'long'}
+    );
+    const hora = new Date().toLocaleTimeString("pt-BR", 
+        { hour: '2-digit', minute: '2-digit' });
+    const data = new Date().toLocaleDateString("pt-BR")
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const dataItem = document.createElement("p");
+    dataItem.innerText = dataCompleta;
+    dataItem.classList.add("texto-data");
+
     containerItemDaLista.appendChild(inputCheckbox);
     containerItemDaLista.appendChild(nomeItem);
-
     itemDaLista.appendChild(containerItemDaLista);
+    itemDaLista.appendChild(dataItem);
     listaDeCompras.appendChild(itemDaLista);
-
+    
+    VerificaListaVazia();
 }) 
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function VerificaListaVazia() {
+    const itemDaLista = listaDeCompras.querySelectorAll("li");
+    // Verifica se a lista está vazia
+    
+    if (itemDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
+
+VerificaListaVazia();
